51d1f610c2e3842f625bb6dfb042077e28fb8372
Oliver Chang <oliverc1622@gmail.com>
2023-12-04 00:28:47 -0800

    expr1 bash script

---
+++ experiment1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+printf "Running Experiment 1: DDQN on C benchmarks"
+
+printf "Trial 11"
+python run-dqn-benchmarks.py 11
+
+printf "Trial 12"
+python run-dqn-benchmarks.py 12
+
+printf "Trial 13"
+python run-dqn-benchmarks.py 13
+
+printf "Trial 14"
+python run-dqn-benchmarks.py 14
